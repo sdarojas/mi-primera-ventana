81a0e54e78b4f396fa328e7fcb9e7bf1de1c44af
sdarojas <sdarojas@gmail.com>
2022-09-16 10:52:24 +0200

    Avance en dosPaneles

--- src/org/iesfm/ventana/dosPaneles.java
+++ src/org/iesfm/ventana/dosPaneles.java
@@ -6,21 +6,22 @@ import java.awt.*;
 public class dosPaneles {
     public static void main(String[] args) {
         JFrame f = new JFrame();
-        f.setBounds(20,20,300,300);
+        f.setBounds(20,20,500,300);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setTitle("Titulo");
 
         JPanel panel1 = new JPanel();
         panel1.setBackground(Color.BLUE);
-        panel1.setBounds(100,5,100,100);
+        panel1.setLayout(null);
         JPanel panel2 = new JPanel();
         panel2.setBackground(Color.CYAN);
-        panel2.setBounds(100,105,500,500);
+        panel2.setBounds(0,0,100,100);
 
         JPanel panel3 = new JPanel();
-        panel3.setBounds(100,200,50,50);
+        panel3.setBounds(200,0,100,100);
         panel3.setBackground(Color.LIGHT_GRAY);
+
         f.add(panel1);
         panel1.add(panel2);
         panel1.add(panel3);
